63dcc856bc58e1fed20f7843dac8a9f1cc2fd90b
sedoisd <saraskin.serafim@yandex.ru>
2025-04-25 23:38:19 +0400

    reg auth набросок v2

--- forms/user.py
+++ forms/user.py
@@ -4,15 +4,10 @@ from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
-    login_or_email = EmailField('Почта', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    name = StringField('Имя', validators=[DataRequired()])
-    surname = StringField('Фамилия', validators=[DataRequired()])
-    age = IntegerField('Возраст', validators=[DataRequired()])
-    position = StringField('Должность', validators=[DataRequired()])
-    speciality = StringField('Специализация', validators=[DataRequired()])
-    address = StringField('Адрес', validators=[DataRequired()])
     submit = SubmitField('Войти')
 
 
